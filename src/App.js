29515b2b14e9f20f1eb994c1d5b38dd12a3f0d29
Adetola Onasanya <adetola.onasanya@gmail.com>
2022-04-13 16:57:02 +0100

    Debounce search query

--- src/App.js
+++ src/App.js
@@ -2,39 +2,37 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 import axios from "axios";
+
+import { debounce } from "./utils";
+
 const url = "https://itunes.apple.com/search?term=";
 const initialList = ["A", "B", "C", "D", "E"];
 
 function App() {
   const [searchKey, setSearchKey] = useState("");
-  const [searchResult, setSearchResult] = useState([...initialList]);
+  const [searchResult, setSearchResult] = useState([]);
   const [displayList, setDisplayList] = useState([...initialList]);
   const [loading, setLoading] = useState(false);
 
-  // create a function to fetch result from url using axios
+  // Fetch results from iTunes API, sort and pick first 5 items
   const fetchResult = async (term) => {
     const result = await axios.get(url + term);
-    // sort results alphabetically and pick first 5
     const sortedResult = result.data.results
       .sort((a, b) => (a.collectionName > b.collectionName ? 1 : -1))
       .slice(5);
 
-    // return collectionName from each result
     setSearchResult(sortedResult.map((item) => item.collectionName));
   };
 
-  const handleSearch = (e) => {
-    // call fetchResult function with searchKey
-    if (e.key === "Enter") {
-      setLoading(true);
-      fetchResult(searchKey);
-    }
+  // call fetchResult function with searchKey
+  const handleSearch = () => {
+    setLoading(true);
+    fetchResult(searchKey);
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (searchResult.length > 0) {
-        // if searchResult is not empty
         setDisplayList([...displayList.slice(1), ...searchResult]);
       } else {
         setDisplayList([...displayList.slice(1), displayList[0]]);
@@ -51,10 +49,10 @@ function App() {
         placeholder="Search Band"
         value={searchKey}
         onChange={(e) => setSearchKey(e.target.value)}
-        onKeyPress={handleSearch}
+        onKeyPress={debounce(() => handleSearch(), 3000)}
       />
       <div className="result-container">
-        {displayList.slice(0, 5).map((result, index) => {
+        {displayList.map((result, index) => {
           return (
             <div className="result" key={index}>
               {result}
